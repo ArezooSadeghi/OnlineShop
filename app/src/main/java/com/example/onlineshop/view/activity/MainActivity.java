6cb141d4af05c4f546f945c68e73ec375345edc4
arezo <arezoo.sdghi@gmail.com>
2021-01-17 16:00:37 +0330

    Define Product class as model. Using pattern repository.

--- app/src/main/java/com/example/onlineshop/view/activity/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/view/activity/MainActivity.java
@@ -19,11 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_category, R.id.navigation_cart)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 }
--- app/src/main/java/com/example/onlineshop/view/fragment/HomeFragment.java
+++ app/src/main/java/com/example/onlineshop/view/fragment/HomeFragment.java
@@ -1,6 +1,63 @@
 package com.example.onlineshop.view.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.example.onlineshop.R;
+import com.example.onlineshop.databinding.FragmentHomeBinding;
+
 public class HomeFragment extends Fragment {
+    private FragmentHomeBinding mBinding;
+
+
+    public static HomeFragment newInstance() {
+        Bundle args = new Bundle();
+        HomeFragment fragment = new HomeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_home,
+                container,
+                false);
+        return mBinding.getRoot();
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.home_menu, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
