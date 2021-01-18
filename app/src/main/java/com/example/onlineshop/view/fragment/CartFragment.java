511df13e91b9a1f57ee8218a78c4c46ec67cdcde
arezo <arezoo.sdghi@gmail.com>
2021-01-18 10:44:52 +0330

    Create RecyclerView in Cart fragment for display purchased products.

--- app/src/main/java/com/example/onlineshop/view/fragment/CartFragment.java
+++ app/src/main/java/com/example/onlineshop/view/fragment/CartFragment.java
@@ -1,7 +1,51 @@
 package com.example.onlineshop.view.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.onlineshop.R;
+import com.example.onlineshop.databinding.FragmentCartBinding;
 
 public class CartFragment extends Fragment {
+    private FragmentCartBinding mBinding;
 
+    public static CartFragment newInstance() {
+        Bundle args = new Bundle();
+        CartFragment fragment = new CartFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_cart,
+                container,
+                false);
+
+        initRecyclerView();
+
+        return mBinding.getRoot();
+    }
+
+    private void initRecyclerView() {
+        mBinding.recyclerViewCart.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 }
