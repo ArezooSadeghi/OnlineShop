029d679b209941c977d3d226d5bf11f0ad372102
arezo <arezoo.sdghi@gmail.com>
2021-01-18 12:06:16 +0330

    Adding new destination to navigation graph for display details of product.

---
+++ app/src/main/java/com/example/onlineshop/view/fragment/DetailFragment.java
@@ -0,0 +1,40 @@
+package com.example.onlineshop.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.onlineshop.R;
+import com.example.onlineshop.databinding.FragmentDetailBinding;
+
+public class DetailFragment extends Fragment {
+    private FragmentDetailBinding mBinding;
+
+    public static DetailFragment newInstance() {
+        DetailFragment fragment = new DetailFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_detail,
+                container,
+                false);
+        return mBinding.getRoot();
+    }
+}
