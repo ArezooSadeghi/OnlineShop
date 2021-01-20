04132930ca9b072515cec20d341c61d5a4ba0d60
arezo <arezoo.sdghi@gmail.com>
2021-01-20 20:50:00 +0330

    Send order with POST request using Retrofit.

--- app/src/main/java/com/example/onlineshop/view/fragment/AddressFragment.java
+++ app/src/main/java/com/example/onlineshop/view/fragment/AddressFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.onlineshop.R;
@@ -27,12 +29,15 @@ public class AddressFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mViewModel = new ViewModelProvider(this).get(AddressViewModel.class);
+        setObserver();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,15 +51,41 @@ public class AddressFragment extends Fragment {
         return mBinding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         AddressFragmentArgs args = AddressFragmentArgs.fromBundle(getArguments());
         String email = args.getEmail();
+        setListener(email);
+    }
+
+
+    private void setObserver() {
+        mViewModel.getStatusCodePostOrderLiveData().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer statusCode) {
+                showReult(statusCode);
+            }
+        });
+    }
+
+
+    private void showReult(Integer statusCode) {
+        if (statusCode == 400) {
+            Toast.makeText(getContext(), R.string.failed_final_registration_order, Toast.LENGTH_LONG).show();
+        }
+        if (statusCode == 201) {
+            Toast.makeText(getContext(), R.string.successful_final_registration_order, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private void setListener(String email) {
         mBinding.btnFinalRegistrationOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                mViewModel.postOrder(email);
             }
         });
     }
