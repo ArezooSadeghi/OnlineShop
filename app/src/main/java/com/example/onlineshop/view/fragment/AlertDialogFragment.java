9c642ec80fbeed4671dcdc360a988b69a990e250
arezo <arezoo.sdghi@gmail.com>
2021-01-20 08:02:09 +0330

    Adding a new destination to navigation graph.

--- app/src/main/java/com/example/onlineshop/view/fragment/AlertDialogFragment.java
+++ app/src/main/java/com/example/onlineshop/view/fragment/AlertDialogFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.onlineshop.R;
 import com.example.onlineshop.databinding.FragmentAlertDialogBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AlertDialogFragment extends DialogFragment {
     private FragmentAlertDialogBinding mBinding;
@@ -44,6 +46,27 @@ public class AlertDialogFragment extends DialogFragment {
                 false);
 
         mBinding.setTotalAmountPaid(getString(R.string.total_amount_paid) + mTotalPrice);
+
+        mBinding.fabOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBinding.txtDiscountCode.getText().toString().equals("")) {
+                    Snackbar.make(view, R.string.empty_discount_code, Snackbar.LENGTH_LONG).show();
+                } else {
+                    if (mBinding.txtDiscountCode.getText().toString().equals("code10")) {
+                        Double finalPrice = Double.valueOf(mTotalPrice) - (Double.valueOf(mTotalPrice) * 0.1);
+                        mBinding.setAmountPaidWithDiscount(getString(R.string.amount_paid_with_discount) + " " + String.valueOf(finalPrice));
+                    }
+                }
+            }
+        });
+
+        mBinding.btnFinalConfirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
 
         return new AlertDialog.Builder(getContext())
                 .setView(mBinding.getRoot())
