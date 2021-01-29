1a25295d739a953b03fccd7493317ba0765fb773
arezo <arezoo.sdghi@gmail.com>
2021-01-29 09:36:43 +0330

    Adding Toolbar to SearchableActivity.

--- app/src/main/java/com/example/onlineshop/view/activity/SearchableActivity.java
+++ app/src/main/java/com/example/onlineshop/view/activity/SearchableActivity.java
@@ -3,7 +3,10 @@ package com.example.onlineshop.view.activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
@@ -13,7 +16,6 @@ import com.example.onlineshop.R;
 import com.example.onlineshop.adapter.SearchAdapter;
 import com.example.onlineshop.databinding.ActivitySearchableBinding;
 import com.example.onlineshop.model.Product;
-import com.example.onlineshop.view.fragment.SortDialogFragment;
 import com.example.onlineshop.viewmodel.SearchableViewModel;
 
 import java.util.List;
@@ -28,13 +30,44 @@ public class SearchableActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_searchable);
 
-        mViewModel = new ViewModelProvider(this).get(SearchableViewModel.class);
-        mBinding.setSearchableViewModel(mViewModel);
-        setObserver();
+        setToolbar();
 
+        mViewModel = new ViewModelProvider(this).get(SearchableViewModel.class);
+        setObserver();
         handleIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.searchable_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_sort_new:
+                mViewModel.searchSortProducts(mQuery, "date", "desc");
+                return true;
+            case R.id.item_sort_best_sellers:
+                mViewModel.searchSortProducts(mQuery, "popularity", "desc");
+                return true;
+            case R.id.item_sort_price_low_high:
+                mViewModel.searchSortProducts(mQuery, "price", "asc");
+                return true;
+            case R.id.item_sort_price_high_low:
+                mViewModel.searchSortProducts(mQuery, "price", "desc");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setToolbar() {
+        setSupportActionBar(mBinding.searchableToolbar);
     }
 
     private void setObserver() {
@@ -42,37 +75,6 @@ public class SearchableActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<Product> products) {
                 setupAdapter(products);
-            }
-        });
-
-        mViewModel.getSortClickedSingleLiveEvent().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean isSortClicked) {
-                if (isSortClicked) {
-                    SortDialogFragment dialogFragment = SortDialogFragment.newInstance();
-                    dialogFragment.show(getSupportFragmentManager(), TAG);
-                }
-            }
-        });
-
-        mViewModel.getSortNewMutableLiveData().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean isNewSortClicked) {
-                mViewModel.searchSortProducts(mQuery, "date", "desc");
-            }
-        });
-
-        mViewModel.getSearchSortProductsLiveData().observe(this, new Observer<List<Product>>() {
-            @Override
-            public void onChanged(List<Product> products) {
-                setupAdapter(products);
-            }
-        });
-
-        mViewModel.getSortBestSellerMutableLiveData().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean isBestSellerClicked) {
-                mViewModel.searchSortProducts(mQuery, "popularity", "desc");
             }
         });
 
--- app/src/main/java/com/example/onlineshop/viewmodel/SearchableViewModel.java
+++ app/src/main/java/com/example/onlineshop/viewmodel/SearchableViewModel.java
@@ -5,20 +5,15 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.onlineshop.model.Product;
 import com.example.onlineshop.repository.ProductRepository;
-import com.example.onlineshop.singleliveevent.SingleLiveEvent;
 
 import java.util.List;
 
 public class SearchableViewModel extends AndroidViewModel {
     private ProductRepository mRepository;
     private LiveData<List<Product>> mSearchProductLiveData;
-    private SingleLiveEvent<Boolean> mSortClickedSingleLiveEvent = new SingleLiveEvent<>();
-    private MutableLiveData<Boolean> mSortNewMutableLiveData = new MutableLiveData<>();
-    private MutableLiveData<Boolean> mSortBestSellerMutableLiveData = new MutableLiveData<>();
     private LiveData<List<Product>> mSearchSortProductsLiveData;
 
     public SearchableViewModel(@NonNull Application application) {
@@ -32,32 +27,8 @@ public class SearchableViewModel extends AndroidViewModel {
         return mSearchProductLiveData;
     }
 
-    public SingleLiveEvent<Boolean> getSortClickedSingleLiveEvent() {
-        return mSortClickedSingleLiveEvent;
-    }
-
-    public void setSortClickedSingleLiveEvent() {
-        mSortClickedSingleLiveEvent.setValue(true);
-    }
-
-    public MutableLiveData<Boolean> getSortNewMutableLiveData() {
-        return mSortNewMutableLiveData;
-    }
-
-    public void setSortNewMutableLiveData() {
-        mSortNewMutableLiveData.setValue(true);
-    }
-
     public LiveData<List<Product>> getSearchSortProductsLiveData() {
         return mSearchSortProductsLiveData;
-    }
-
-    public MutableLiveData<Boolean> getSortBestSellerMutableLiveData() {
-        return mSortBestSellerMutableLiveData;
-    }
-
-    public void setSortBestSellerMutableLiveData() {
-        mSortBestSellerMutableLiveData.setValue(true);
     }
 
     public void searchSortProducts(String search, String orderby, String order) {
